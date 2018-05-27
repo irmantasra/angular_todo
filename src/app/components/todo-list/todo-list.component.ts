d1bdf55a4387589cf7618e8f81e99ec2a2fb4648
IrmantasRamanauskas <irmantasra@gmail.com>
2018-05-27 09:35:01 +0300

    add create, delete, toggle status functionality

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import shortid from 'shortid';
+
 import todosData from '../../../data/todoItems';
 
 @Component({
@@ -9,16 +11,44 @@ import todosData from '../../../data/todoItems';
 })
 export class TodoListComponent implements OnInit {
 	todoItems:Item[];
+	newTitle:string = '';
   constructor() { }
 
   ngOnInit() {
 		this.todoItems = todosData;
-  }
+	}
+	
+	handleInputChange(title){
+		this.newTitle = title;
+		return false;
+	}
+	
+	addTodo() {
+		this.todoItems.unshift({
+			id: shortid.generate(),
+			title: this.newTitle,
+			completed: false
+		});
+		this.newTitle = '';
+		return false;
+	}
 
+	deleteTodo(id) {
+		this.todoItems.map((item, index) => {
+			if (item.id === id) this.todoItems.splice(index, 1);
+		});
+	}
+
+	toggleTodoStatus(id) {
+		this.todoItems = this.todoItems.map(item =>{
+			if(item.id === id) return {id: item.id, title: item.title, completed: !item.completed};
+			return item;
+		})
+	}
 }
 
 interface Item {
-	id:number,
+	id:string,
 	title:string,
 	completed:boolean
 }
--- src/data/todoItems.ts
+++ src/data/todoItems.ts
@@ -1,21 +1,21 @@
 export default [
     {
-        id: 0,
+        id: '0',
         title: 'Walk the dog',
         completed: false
     },
     {
-        id: 1,
+        id: '1',
         title: 'Take out the trash',
         completed: true
     },
     {
-        id: 2,
+        id: '2',
         title: 'Vacuum the carpet',
         completed: true
     },
     {
-        id: 3,
+        id: '3',
         title: 'Take out a person',
         completed: false
     }
